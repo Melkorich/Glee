16de68a824c4af4c32094166e98ce14f1b1257c4
Melkorich <Kirill.Lavrov92@gmail.com>
2021-05-25 22:59:31 +0300

    video block and section insights added

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
-const del = require('del')
+const del = require('del');
 const browserSync = require('browser-sync').create();
 
 function browsersync(){
@@ -28,13 +28,14 @@ function styles(){
     .pipe(browserSync.stream())
 }
 
-
 // .pipe(concat('node_modules/animate.css/animate.css'))
 
 function scripts(){
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    // 'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
